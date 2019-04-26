92f36805792acaf8bf13fe2193ec0b00535c357d
boris <boris.matveychuck@gmail.com>
2019-04-26 19:09:48 +0300

    - taxt notes application

---
+++ AppXamarinFormsQuiz/AppXamarinFormsQuiz/NoteEntryPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AppXamarinFormsQuiz.Models;
+using System;
+using System.IO;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppXamarinFormsQuiz
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if(string.IsNullOrWhiteSpace(note.Filename))
+            {
+                var fileName = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(fileName, note.Text);
+            }
+            else
+            {
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        async private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if(File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+
+            await Navigation.PopAsync();
+        }
+    }
+}
